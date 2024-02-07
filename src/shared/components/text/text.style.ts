e8e17b06e7a6900e2764ad6ac17fb2917cab3ddf
VitorLopes02 <vitinhoolopes.vl@gmail.com>
2024-02-06 22:19:44 -0300

    feat: styled types button and text

--- src/shared/components/text/text.style.ts
+++ src/shared/components/text/text.style.ts
@@ -2,12 +2,13 @@ import styled from "styled-components/native"
 
 interface ContainerTextProps {
     color?: string;
-    fontSize: string
+    fontSize: string;
+    fontFamily: 'Poppins-Bold' | 'Poppins-Light' | 'Poppins-Regular';
 }
 
 export const ContainerText = styled.Text<ContainerTextProps>`
     ${((props: { color: any; }) => props.color ? `color: ${props.color}` : "")};
 
-    font-family: Poppins-Bold;
+    font-family: ${(props: { fontFamily: any; }) => props.fontFamily};
     font-size: ${(props: { fontSize: any; }) => props.fontSize}
 `;
