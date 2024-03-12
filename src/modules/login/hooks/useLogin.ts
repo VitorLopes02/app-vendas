3b4764b9267c3a1e5a80b35b324c099040f2416f
VitorLopes02 <vitinhoolopes.vl@gmail.com>
2024-03-11 21:21:09 -0300

    feat: create hook use effect

--- src/modules/login/hooks/useLogin.ts
+++ src/modules/login/hooks/useLogin.ts
@@ -1,25 +1,18 @@
 import { NativeSyntheticEvent, TextInputChangeEventData } from "react-native";
 import { useState } from "react";
-import { connectionAPIPost } from "../../../shared/functions/connection/connectionAPI";
+import { useRequest } from "../../../shared/hooks/useRequest";
 
 export const useLogin = () => {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
-    const [loading, setLoading] = useState<boolean>(false);
-    const [errorMessage, setErrorMessage] = useState<string>('');
+    const { authRequest, errorMessage, loading, setErrorMessage } = useRequest();     
 
     const handleOnPress = async () => {
-        setLoading(true);
-        await connectionAPIPost('http://172.19.128.1:8080/auth', {
-            email, 
-            password
-        }).catch(() => {
-            setErrorMessage('Usuário ou senha inválidos')
-        }) ;
-        setLoading(false);
-        console.log("Clico");
-    
-    }
+        authRequest({
+            email,
+            password,
+        });
+    };
 
     const handleOnChangeEmail = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
         setErrorMessage('');
